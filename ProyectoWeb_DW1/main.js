b8f7d1824d4e95ccd6ad3abf558f1443d6718801
daniel valle <danielvallec98@gmail.com>
2025-10-06 15:40:21 -0600

    actualice

--- ProyectoWeb_DW1/main.js
+++ ProyectoWeb_DW1/main.js
@@ -32,3 +32,11 @@ function padZero(num) {
 
 setInterval(updateClock, 1000);
 updateClock();
+
+// --- Menú responsive ---
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
